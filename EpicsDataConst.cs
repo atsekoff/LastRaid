e0be51b8c66558ff4b2c47af63ffc6c90b433b51
atsekoff <atsekoff@gmail.com>
2022-05-16 02:58:36 +0200

    Added sub/noble bosses to the tod list

--- EpicsDataConst.cs
+++ EpicsDataConst.cs
@@ -8,9 +8,9 @@ namespace LastRaid
     public const int DEFAULT_HEADSUP_MINUTES = 30;
 
     // rb data
-    public enum BossNames { QueenAnt, Core, Orfen, Zaken, Baium, Antharas, Valakas, Frintezza }
-    public static readonly int[] DEATH_DURATIONS = { 24, 30, 30, 40, 120, 192, 264, 48 };
-    public static readonly int[] WINDOW_DURATIONS = { 6, 6, 6, 8, 8, 8, 0, 2 };
+    public enum BossNames { QueenAnt, Core, Orfen, Zaken, Baium, Antharas, Valakas, Frintezza, Barakiel, Cabrio, Hallate, Kernon, Golkonda }
+    public static readonly int[] DEATH_DURATIONS = { 24, 30, 30, 40, 120, 192, 264, 48, 12, 12, 12, 12, 12 };
+    public static readonly int[] WINDOW_DURATIONS = { 6, 6, 6, 8, 8, 8, 0, 2, 24, 24, 24, 24, 24 };
     public static readonly string[] EPIC_THUMBNAILS = {
       "https://lineage.pmfun.com/data/img/accessory_ring_of_queen_ant_i00.png",
       "https://lineage.pmfun.com/data/img/accessory_ring_of_core_i00.png",
@@ -19,7 +19,12 @@ namespace LastRaid
       "https://lineage.pmfun.com/data/img/accessory_ring_of_baium_i00.png",
       "https://lineage.pmfun.com/data/img/accessory_earring_of_antaras_i00.png",
       "https://lineage.pmfun.com/data/img/accessory_necklace_of_valakas_i00.png",
-      "https://lineage.pmfun.com/data/img/accessory_necklace_of_frintessa_i00.png"};
+      "https://lineage.pmfun.com/data/img/accessory_necklace_of_frintessa_i00.png",
+      "https://lineage.pmfun.com/data/img/accessory_noblesse_tiara_i00_0.png",
+      "https://lineage.pmfun.com/data/img/etc_bead_white_i00.png",
+      "https://lineage.pmfun.com/data/img/etc_imperial_scepter_i00.png",
+      "https://lineage.pmfun.com/data/img/etc_imperial_scepter_i02.png",
+      "https://lineage.pmfun.com/data/img/etc_imperial_scepter_i01.png" };
 
     // component
     public const string CANCEL_BUTTON_LABEL = "Cancel";
